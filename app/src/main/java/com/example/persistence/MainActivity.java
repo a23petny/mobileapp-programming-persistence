7d3d14b0c34f268c86f0fca545505f881e6b864b
a23petny <a23petny@student.his.se>
2024-05-06 13:36:18 +0200

    fond the uiview elements

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.persistence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editText1;
+    private EditText editText2;
+    private EditText editText3;
+
+
+    private Button read;
+    private Button write;
+
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText1 = findViewById(R.id.editTextText1);
+        editText2 = findViewById(R.id.editTextText2);
+        editText3 = findViewById(R.id.editTextText3);
+
+        read = findViewById(R.id.Read);
+
+        write = findViewById(R.id.Write);
+        textView = findViewById(R.id.textView1);
+
     }
 }
+
