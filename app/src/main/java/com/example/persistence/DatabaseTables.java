1aad4a6879f1ad8047a255d7bf300d91fbe75e86
a23petny <a23petny@student.his.se>
2024-05-07 11:50:46 +0200

    fixed the table

--- app/src/main/java/com/example/persistence/DatabaseTables.java
+++ app/src/main/java/com/example/persistence/DatabaseTables.java
@@ -19,7 +19,6 @@ class DatabaseTables {
         static final String COLUMN_NAME_FOBONE = "fobone";
         static final String COLUMN_NAME_FOBTWO = "fobtwo";
         static final String COLUMN_NAME_FOBTHREE = "fobthree";
-
         static final String COLUMN_NAME_SIZE = "size";
         public final long id;
         public final String fob1;
